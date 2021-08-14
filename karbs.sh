e00f8315f758a0f776f94ceb941d71e61d746465
kuring <361118339@qq.com>
2021-08-15 00:16:37 +0800

    add fcitx5...

--- karbs.sh
+++ karbs.sh
@@ -264,6 +264,8 @@ EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
 grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
 	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
 
+# Set some programs to start automatically
+
 # Start/restart PulseAudio.
 killall pulseaudio; sudo -u "$name" pulseaudio --start
 
